bd16c996d17ce4ab2cb81ab1c8608c5c457e1fa6
Aditya <Adityakasera66@gmail.com>
2026-01-18 22:34:59 +0530

    effect add by gsap and sheryjs

--- script.js
+++ script.js
@@ -1,37 +1,35 @@
-
-
-// Shery.imageEffect("#back", {
-//   style: 5,
-//   config: {
-//     a: { value: 1.6, range: [0, 30] },
-//     b: { value: -0.91, range: [-1, 1] },
-//     aspect: { value: 1.797752808988764 },
-//     ignoreShapeAspect: { value: true },
-//     shapePosition: { value: { x: 0, y: 0 } },
-//     shapeScale: { value: { x: 0.5, y: 0.5 } },
-//     shapeEdgeSoftness: { value: 0 },
-//     shapeRadius: { value: 0 },
-//     currentScroll: { value: 0 },
-//     scrollLerp: { value: 0.07 },
-//     gooey: { value: true },
-//     infiniteGooey: { value: true },
-//     growSize: { value: 4 },
-//     durationOut: { value: 1 },
-//     durationIn: { value: 1 },
-//     displaceAmount: { value: 0.5 },
-//     masker: { value: true },
-//     maskVal: { value: 1.03 },
-//     scrollType: { value: 0 },
-//     geoVertex: { value: 1 },
-//     noEffectGooey: { value: false },
-//     onMouse: { value: 1 },
-//     noise_speed: { value: 0.76 },
-//     metaball: { value: 0.2 },
-//     discard_threshold: { value: 0.5 },
-//     antialias_threshold: { value: 0 },
-//     noise_height: { value: 0.5 },
-//     noise_scale: { value: 10.09 }
-//   },
-//   gooey: true
-// });
+Shery.imageEffect("#back", {
+  style: 5,
+  config: {
+    a: { value: 1.6, range: [0, 30] },
+    b: { value: -0.91, range: [-1, 1] },
+    aspect: { value: 1.797752808988764 },
+    ignoreShapeAspect: { value: true },
+    shapePosition: { value: { x: 0, y: 0 } },
+    shapeScale: { value: { x: 0.5, y: 0.5 } },
+    shapeEdgeSoftness: { value: 0 },
+    shapeRadius: { value: 0 },
+    currentScroll: { value: 0 },
+    scrollLerp: { value: 0.07 },
+    gooey: { value: true },
+    infiniteGooey: { value: true },
+    growSize: { value: 4 },
+    durationOut: { value: 1 },
+    durationIn: { value: 1 },
+    displaceAmount: { value: 0.5 },
+    masker: { value: true },
+    maskVal: { value: 1.03 },
+    scrollType: { value: 0 },
+    geoVertex: { value: 1 },
+    noEffectGooey: { value: false },
+    onMouse: { value: 1 },
+    noise_speed: { value: 0.76 },
+    metaball: { value: 0.2 },
+    discard_threshold: { value: 0.5 },
+    antialias_threshold: { value: 0 },
+    noise_height: { value: 0.5 },
+    noise_scale: { value: 10.09 }
+  },
+  gooey: true
+});
 
